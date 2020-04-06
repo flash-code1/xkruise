06d7128f80aab9881113e6216daf9bf596eb0651
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-06 23:32:56 +0100

    make it

--- services.php
+++ services.php
@@ -1,17 +1,8 @@
 <!DOCTYPE html>
 <html class="wide wow-animation" lang="en">
-  <head>
-    <title>Services</title>
-    <meta name="viewport" content="width=device-width height=device-height initial-scale=1.0">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Work+Sans:300,400,500,700%7CZilla+Slab:300,400,500,700,700i%7CGloria+Hallelujah">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/fonts.css">
-    <link rel="stylesheet" href="css/style.css">
-    <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
-  </head>
+<?php
+  include("header.php");
+  ?>
   <body>
     <div class="ie-panel"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
     <div class="preloader">
@@ -39,7 +30,7 @@
                     </li>
                     <li class="rd-nav-item active"><a class="rd-nav-link" href="services.php">Services</a>
                     </li>
-                    <li class="rd-nav-item"><a class="rd-nav-link" href="typography.php">Typography</a>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="book.php">Book</a>
                     </li>
                     <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.php">Contacts</a>
                     </li>
