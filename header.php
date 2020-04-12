aa33fabc4007990037a8a9d35f9a2156b24ad8f6
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-12 09:16:16 +0100

    check first

--- header.php
+++ header.php
@@ -8,5 +8,29 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/fonts.css">
     <link rel="stylesheet" href="css/style.css">
+    <!-- for login -->
+    <link href="assetsk/css/bootstrap.css" rel="stylesheet" />
+
+	<link href="assetsk/css/login-register.css" rel="stylesheet" />
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+	<script src="assetsk/js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="assetsk/js/bootstrap.js" type="text/javascript"></script>
+	<script src="assetsk/js/login-register.js" type="text/javascript"></script>
+
+
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- Material Kit CSS -->
+  <link href="../assets/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
+  <!-- accordion -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+  <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="../datatable/jquery-3.3.2.js"></script>
+  <script src="../datatable/jquery.dataTables.min.js"></script>
+  <script src="../datatable/dataTables.bootstrap.min.js"></script>
+  <script src="../datatable/DropdownSelect.js"></script>
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.3/materia/bootstrap.min.css"> -->
     <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
   </head>
--- login.php
+++ login.php
@@ -1,32 +1,21 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Login/Register</title>
-
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<style>body{padding-top: 60px;}</style>
-
-    <link href="assetsk/css/bootstrap.css" rel="stylesheet" />
-
-	<link href="assetsk/css/login-register.css" rel="stylesheet" />
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-
-	<script src="assetsk/js/jquery-1.10.2.js" type="text/javascript"></script>
-	<script src="assetsk/js/bootstrap.js" type="text/javascript"></script>
-	<script src="assetsk/js/login-register.js" type="text/javascript"></script>
-
+	<style>body{padding-top: 60px;}</style> 
+    <?php include('header.php'); ?>
 </head>
 <body>
     <div class="container">
-        <div class="row">
+        <!-- <div class="row">
             <div class="col-sm-4"></div>
             <div class="col-sm-4">
                  <a class="btn big-login" data-toggle="modal" href="javascript:void(0)" onclick="openLoginModal();">Log in</a>
                  <a class="btn big-register" data-toggle="modal" href="javascript:void(0)" onclick="openRegisterModal();">Register</a></div>
             <div class="col-sm-4"></div>
-        </div>
+        </div> -->
 
 
 		 <div class="modal fade login" id="loginModal">
