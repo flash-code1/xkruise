ba53a00dbd3a4fddf5e78c1287f4a362634022ba
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-12 20:30:43 +0100

    check

--- ERP/header.php
+++ ERP/header.php
@@ -133,3 +133,57 @@ input[type=number] {
       </div>
     </div>
       <!-- Navbar -->
+
+      <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <?php
+              if($page_title == "Dashboard"){
+            ?>
+            <!-- <a class="btn btn-primary" href="#pablo"><i class="fa fa-caret-left"></i> Back</a> -->
+            <?php
+              }else{            ?>
+
+              <a class="btn btn-primary" href="<?php echo $destination ?>"><i class="fa fa-caret-left"></i> Back</a>
+              
+              <?php
+                }
+              ?>
+          
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#pablo">
+                  <i class="material-icons">notifications</i>
+                </a>
+              </li>
+              <!-- user setup -->
+              <li class="nav-item dropdown">
+                <a class="nav-link" href="#pablo" id="navbarDropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="material-icons">person</i>
+                  <!-- Insert user display name here -->
+                  <!-- <p class="d-lg-none d-md-block"> -->
+                    <?php echo $_SESSION["username"]; ?>
+                  <!-- </p> -->
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
+                  <a class="dropdown-item" href="#">Profile</a>
+                  <a class="dropdown-item" href="#">Settings</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="../functions/logout.php">Log out</a>
+                </div>
+              </li>
+              <!-- your navbar here -->
+            </ul>
+          </div>
+        </div>
+      </nav>
