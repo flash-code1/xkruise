fc0641aaf11909128c486ac6df5adfca054fbab0
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-13 10:15:06 +0100

    plkjh

--- functions/clientreg.php
+++ functions/clientreg.php
@@ -12,7 +12,7 @@ $hash = password_hash($password, PASSWORD_DEFAULT);
 $usertype = "client";
 $status = "Not Active";
 
-$res = mysqli_query($connection, "SELECT * FROM users WHERE username = '$username' && email = '$password'");
+$res = mysqli_query($connection, "SELECT * FROM users");
 
 if (count([$res]) == 1) {
     $x = mysqli_fetch_array($res);
